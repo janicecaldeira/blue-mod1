aebca05fde9e5c1cc0b7f1867af74cc7a0482588
Janice Caldeira <janicecaldeira@gmail.com>
2021-06-15 19:50:34 -0300

    Exemplo de um caixa eletrônico genérico

--- Caixa_eletronico.py
+++ Caixa_eletronico.py
@@ -4,19 +4,19 @@ while saque < 10 or saque > 600:
     saque = float(input("Digite o valor que deseja sacar (entre R$10 e R$600): ").replace(",","."))
 else:
     print(f"\nValor de saque liberado R$ {int(saque)}")
-    cem = int(saque/100)
+    cem = int(saque//100)
     saque = saque % 100
 
-    cinquenta = int(saque/50)
+    cinquenta = int(saque//50)
     saque = saque % 50
 
-    dez = int(saque/10)
+    dez = int(saque//10)
     saque = saque % 10
 
-    cinco = int(saque/5)
+    cinco = int(saque//5)
     saque = saque % 5
 
-    um = int(saque/1)
+    um = int(saque//1)
     saque = saque % 1
 
 print("\nSerão fornecidas:")
