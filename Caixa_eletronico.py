081d555eb0cd5e08ad133d325be2ed9b563edc01
Janice Caldeira <janicecaldeira@gmail.com>
2021-06-15 20:44:23 -0300

    Exemplo de caixa eletrônico

--- Caixa_eletronico.py
+++ Caixa_eletronico.py
@@ -1,28 +1,37 @@
-saque = float(input("Digite o valor que deseja sacar (entre R$10 e R$600): ").replace(",","."))
+saque = float(input("Qual o valor você deseja sacar (entre R$10 e R$600): ").replace(",","."))
 
 while saque < 10 or saque > 600:
-    saque = float(input("Digite o valor que deseja sacar (entre R$10 e R$600): ").replace(",","."))
+    saque = float(input("Por favor, informe um valor entre R$10 e R$600: ").replace(",","."))
 else:
-    print(f"\nValor de saque liberado R$ {int(saque)}")
-    cem = int(saque//100)
+    print(f"\n\U0001F4A1 Valor de saque liberado: R$ {int(saque)}")
+    cem = saque//100
     saque = saque % 100
 
-    cinquenta = int(saque//50)
+    cinquenta = saque//50
     saque = saque % 50
 
-    dez = int(saque//10)
+    dez = saque//10
     saque = saque % 10
 
-    cinco = int(saque//5)
+    cinco = saque//5
     saque = saque % 5
 
-    um = int(saque//1)
+    um = saque//1
     saque = saque % 1
 
 print("\nSerão fornecidas:")
 
-print(f"{um} notas de R$ 1,00")
-print(f"{cinco} notas de R$ 5,00")
-print(f"{dez} notas de R$ 10,00")
-print(f"{cinquenta} notas de R$ 50,00")
-print(f"{cem} notas de R$ 100,00")
+if cem > 0:
+    print(f"\U0001F4B2 {int(cem)} nota(s) de R$ 100,00")
+
+if cinquenta > 0:
+    print(f"\U0001F4B2 {int(cinquenta)} nota(s) de R$ 50,00")
+
+if dez > 0:
+    print(f"\U0001F4B2 {int(dez)} nota(s) de R$ 10,00")
+
+if cinco > 0:
+    print(f"\U0001F4B2 {int(cinco)} nota(s) de R$ 5,00")
+
+if um > 0:
+    print(f"\U0001F4B2 {int(um)} nota(s) de R$ 1,00")
